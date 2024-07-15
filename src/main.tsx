a35b70046ad1a80881a9964028914529f54e1dea
sojib076 <sojibds332@gmail.com>
2024-07-15 23:33:22 +0600

    notification

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
 import { Toaster } from "sonner";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Notification from "./Notification.tsx";
 const router =createBrowserRouter([
-      {
-        path: "/",
-        element: <App />,
-      },
 
       {
         path: "/notification",
