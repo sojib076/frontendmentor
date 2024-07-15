39f2737e2d5fd56a7983b6599b06d38dd04bfeb8
sojib076 <sojibds332@gmail.com>
2024-07-16 01:12:28 +0600

    modalworking

--- src/Newsletter.tsx
+++ src/Newsletter.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react';
 import notifiimage from '@/assets/illustration-sign-up-desktop.svg';
 import icons from '@/assets/icon-list.svg';
 
@@ -7,12 +8,12 @@ const Newsletter = () => {
     const [error, setError] = useState('');
     const [submitted, setSubmitted] = useState(false);
 
-    const validateEmail = (email) => {
+    const validateEmail = (email:string) => {
         const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return re.test(String(email).toLowerCase());
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e:any) => {
         e.preventDefault();
         if (!validateEmail(email)) {
             setError('Invalid email address');
@@ -32,7 +33,7 @@ const Newsletter = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-500 font-Roboto">
             {!submitted ? (
-                <div className="w-[60%] p-8 bg-white shadow-md rounded-lg flex justify-between">
+                <div className="lg:-[60%] p-8 bg-white shadow-md rounded-lg flex lg:flex-row flex-col-reverse ">
                     <div className="pl-8">
                         <h2 className="font-extrabold text-4xl">Stay Updated!</h2>
                         <p className="text-sm py-2">
@@ -68,19 +69,19 @@ const Newsletter = () => {
                             <button type="submit" className="bg-[#2A292F] text-white p-2 rounded-lg">Subscribe</button>
                         </form>
                     </div>
-                    <img src={notifiimage} alt="Notification" className="w-[300px]" />
+                    <img src={notifiimage} alt="Notification" className="lg:w-[300px] mt-2 w-full" />
                 </div>
             ) : (
-                <div className="w-[50%] h-auto bg-white p-10 rounded-2xl">
+                <div className="lg:w-[50%] h-auto bg-white lg:p-10 p-6 rounded-2xl">
                     <img src={icons} alt="" className="w-20 py-5" />
-                    <h1 className="text-5xl text-black w-[60%] font-Roboto font-bold">Thank you for subscribing</h1>
-                    <p className="w-[80%] py-5">
+                    <h1 className="lg:text-5xl text-4xl text-black lg:w-[60%] font-Roboto font-bold">Thank you for subscribing</h1>
+                    <p className="w-[80%] py-5  ">
                         A confirmation email has been sent to your {email} address. Please click on the confirmation link to confirm your subscription.
                     </p>
                     <button
                         type="button"
                         onClick={handleDismiss}
-                        className="bg-[#2A292F] text-white p-2 rounded-lg w-[90%] mx-auto hover:scale-90 transform transition duration-300 ease-in-out"
+                        className="bg-[#2A292F] lg:mt-0 mt-52 text-white p-2 rounded-lg w-[90%] mx-auto hover:scale-90 transform transition duration-300 ease-in-out"
                     >
                         Dismiss Message
                     </button>
