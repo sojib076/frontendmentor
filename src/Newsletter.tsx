624a158fc60f63cfab510bdc7925285d2b964489
sojib076 <sojibds332@gmail.com>
2024-07-16 18:24:40 +0600

    colors

--- src/Newsletter.tsx
+++ src/Newsletter.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 import notifiimage from '@/assets/illustration-sign-up-desktop.svg';
+import moblieimage from '@/assets/illustration-sign-up-mobile.svg'
 import icons from '@/assets/icon-list.svg';
 
 const Newsletter = () => {
@@ -33,30 +34,30 @@ const Newsletter = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-500 font-Roboto">
             {!submitted ? (
-                <div className="lg:-[60%] p-8 bg-white shadow-md rounded-lg flex lg:flex-row flex-col-reverse ">
-                    <div className="pl-8">
-                        <h2 className="font-extrabold text-4xl">Stay Updated!</h2>
-                        <p className="text-sm py-2">
-                            Join 60,00+ products managers receiving our monthly newsletter updates on
+                <div className="lg:-[90%] lg:p-8 bg-[white] shadow-md rounded-lg flex lg:flex-row flex-col-reverse ">
+                    <div className="lg:pl-8 pl-5 lg:mt-0 mt-5">
+                        <h2 className="font-extrabold text-4xl text-[#232643]">Stay Updated!</h2>
+                        <p className="text-[16px] py-2 w-[90%] ">
+                            Join 60,00+ products managers receiving  monthly  updates on
                         </p>
-                        <ul className="my-5 text-sm">
+                        <ul className="lg:my-5 my-3 text-sm px-3 ">
                             <li className="flex items-center gap-x-2 my-3">
                                 <img src={icons} alt="icons" className="w-[18px]" />
                                 <span>Product discovery and building what matters</span>
                             </li>
                             <li className="flex items-center gap-x-2 my-3">
                                 <img src={icons} alt="icons" className="w-[18px]" />
-                                <span>Product discovery and building what matters</span>
+                                <span>Measuring to ensure updates are a success</span>
                             </li>
                             <li className="flex items-center gap-x-2 my-3">
                                 <img src={icons} alt="icons" className="w-[18px]" />
-                                <span>Product discovery and building what matters</span>
+                                <span>And much more </span>
                             </li>
                         </ul>
-                        <form onSubmit={handleSubmit} className="flex flex-col w-[88%]">
+                        <form onSubmit={handleSubmit} className="flex flex-col w-[90%] lg:my-0 my-10 lg:pl-0 pl-4">
                             <div className="flex justify-between">
-                                <label htmlFor="email" className="text-sm">Email</label>
-                                {error && <span className="text-red-500 text-sm">{error}</span>}
+                                <label htmlFor="email" className="text-sm font-bold">Email</label>
+                                {error && <span className="text-red-500 text-sm font-bold" >{error}</span>}
                             </div>
                             <input
                                 type="text"
@@ -64,24 +65,25 @@ const Newsletter = () => {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                                 placeholder="email@company.com"
-                                className={`w-[100%] p-2 my-2 border-2 ${error ? 'border-red-500' : 'border-black'} rounded-lg`}
+                                className={`w-[100%] px-4 py-3 my-2 border-2 ${error ? 'border-red-500' : 'border-black'} rounded-lg`}
                             />
-                            <button type="submit" className="bg-[#2A292F] text-white p-2 rounded-lg">Subscribe</button>
+                            <button type="submit" className="bg-[#FF6257] hover:bg-[#232643] text-white p-4 rounded-lg  my-5  hover:scale-90 transform transition duration-300 ease-in-out  ">Subscribe to monthly newsletter </button>
                         </form>
                     </div>
-                    <img src={notifiimage} alt="Notification" className="lg:w-[300px] mt-2 w-full" />
+                    <img src={notifiimage} alt="Notification" className="lg:w-[300px] lg:block hidden mt-2 w-full" />
+                    <img src={moblieimage} alt="Notification" className=" w-full lg:hidden block " />
                 </div>
             ) : (
-                <div className="lg:w-[50%] h-auto bg-white lg:p-10 p-6 rounded-2xl">
-                    <img src={icons} alt="" className="w-20 py-5" />
-                    <h1 className="lg:text-5xl text-4xl text-black lg:w-[60%] font-Roboto font-bold">Thank you for subscribing</h1>
-                    <p className="w-[80%] py-5  ">
+                <div className="lg:w-[35%] h-auto bg-white lg:p-10 p-6 rounded-2xl">
+                    <img src={icons} alt="" className="w-16 " />
+                    <h1 className="lg:text-3xl text-2xl text-gray-950 lg:w-[60%] font-Roboto font-bold py-5">Thanks  for subscribing ! </h1>
+                    <p className="w-[100%]   ">
                         A confirmation email has been sent to your {email} address. Please click on the confirmation link to confirm your subscription.
                     </p>
                     <button
                         type="button"
                         onClick={handleDismiss}
-                        className="bg-[#2A292F] lg:mt-0 mt-52 text-white p-2 rounded-lg w-[90%] mx-auto hover:scale-90 transform transition duration-300 ease-in-out"
+                        className="bg-[#FF6257] hover:bg-[#232643] lg:mt-5 mt-52 text-white p-3 rounded-lg w-[95%] mx-auto hover:scale-90 transform transition duration-300 ease-in-out "
                     >
                         Dismiss Message
                     </button>
